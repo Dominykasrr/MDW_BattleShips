9e3d1b9cc1bc35ae9b6cc9c8f854f9ee41efb7d6
Leo9012 <2334888094@qq.com>
2014-12-15 17:30:02 +0100

    Initial commit

---
+++ BattleShipService/BattleShipClient/Battle Ship.Designer.cs
@@ -0,0 +1,222 @@
+﻿namespace BattleShipClient
+{
+    partial class Battle_Ship
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Battle_Ship));
+            this.pbPlayer2 = new System.Windows.Forms.PictureBox();
+            this.btnReady = new System.Windows.Forms.Button();
+            this.pbPlayer1 = new System.Windows.Forms.PictureBox();
+            this.block1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnDestory = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.block4 = new System.Windows.Forms.Button();
+            this.block3 = new System.Windows.Forms.Button();
+            this.block2 = new System.Windows.Forms.Button();
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.btnOnlonePlayers = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pbPlayer2
+            // 
+            this.pbPlayer2.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pbPlayer2.Location = new System.Drawing.Point(453, 9);
+            this.pbPlayer2.Name = "pbPlayer2";
+            this.pbPlayer2.Size = new System.Drawing.Size(266, 256);
+            this.pbPlayer2.TabIndex = 23;
+            this.pbPlayer2.TabStop = false;
+            // 
+            // btnReady
+            // 
+            this.btnReady.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnReady.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnReady.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnReady.Location = new System.Drawing.Point(15, 284);
+            this.btnReady.Name = "btnReady";
+            this.btnReady.Size = new System.Drawing.Size(118, 46);
+            this.btnReady.TabIndex = 24;
+            this.btnReady.Text = "Ready";
+            this.btnReady.UseVisualStyleBackColor = true;
+            // 
+            // pbPlayer1
+            // 
+            this.pbPlayer1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pbPlayer1.Location = new System.Drawing.Point(166, 9);
+            this.pbPlayer1.Name = "pbPlayer1";
+            this.pbPlayer1.Size = new System.Drawing.Size(266, 256);
+            this.pbPlayer1.TabIndex = 22;
+            this.pbPlayer1.TabStop = false;
+            // 
+            // block1
+            // 
+            this.block1.Location = new System.Drawing.Point(23, 22);
+            this.block1.Name = "block1";
+            this.block1.Size = new System.Drawing.Size(39, 43);
+            this.block1.TabIndex = 2;
+            this.block1.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Image = ((System.Drawing.Image)(resources.GetObject("label5.Image")));
+            this.label5.Location = new System.Drawing.Point(30, 606);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(46, 14);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Destory";
+            // 
+            // btnDestory
+            // 
+            this.btnDestory.Image = ((System.Drawing.Image)(resources.GetObject("btnDestory.Image")));
+            this.btnDestory.Location = new System.Drawing.Point(19, 530);
+            this.btnDestory.Name = "btnDestory";
+            this.btnDestory.Size = new System.Drawing.Size(72, 73);
+            this.btnDestory.TabIndex = 6;
+            this.btnDestory.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.block4);
+            this.panel1.Controls.Add(this.block3);
+            this.panel1.Controls.Add(this.block2);
+            this.panel1.Controls.Add(this.block1);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.btnDestory);
+            this.panel1.Location = new System.Drawing.Point(15, 9);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(130, 256);
+            this.panel1.TabIndex = 25;
+            // 
+            // block4
+            // 
+            this.block4.Location = new System.Drawing.Point(23, 194);
+            this.block4.Name = "block4";
+            this.block4.Size = new System.Drawing.Size(95, 43);
+            this.block4.TabIndex = 15;
+            this.block4.UseVisualStyleBackColor = true;
+            // 
+            // block3
+            // 
+            this.block3.Location = new System.Drawing.Point(23, 134);
+            this.block3.Name = "block3";
+            this.block3.Size = new System.Drawing.Size(72, 43);
+            this.block3.TabIndex = 14;
+            this.block3.UseVisualStyleBackColor = true;
+            // 
+            // block2
+            // 
+            this.block2.Location = new System.Drawing.Point(23, 75);
+            this.block2.Name = "block2";
+            this.block2.Size = new System.Drawing.Size(54, 43);
+            this.block2.TabIndex = 13;
+            this.block2.UseVisualStyleBackColor = true;
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnNewGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnNewGame.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnNewGame.Location = new System.Drawing.Point(151, 284);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(118, 46);
+            this.btnNewGame.TabIndex = 27;
+            this.btnNewGame.Text = "New Game";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            // 
+            // btnOnlonePlayers
+            // 
+            this.btnOnlonePlayers.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnOnlonePlayers.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnOnlonePlayers.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnOnlonePlayers.Location = new System.Drawing.Point(287, 284);
+            this.btnOnlonePlayers.Name = "btnOnlonePlayers";
+            this.btnOnlonePlayers.Size = new System.Drawing.Size(160, 46);
+            this.btnOnlonePlayers.TabIndex = 26;
+            this.btnOnlonePlayers.Text = "Online Players";
+            this.btnOnlonePlayers.UseVisualStyleBackColor = true;
+            this.btnOnlonePlayers.Click += new System.EventHandler(this.btnOnlonePlayers_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnStop.ForeColor = System.Drawing.SystemColors.WindowFrame;
+            this.btnStop.Location = new System.Drawing.Point(463, 284);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(118, 46);
+            this.btnStop.TabIndex = 28;
+            this.btnStop.Text = "Stop Game";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // Battle_Ship
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(734, 339);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.pbPlayer2);
+            this.Controls.Add(this.btnReady);
+            this.Controls.Add(this.pbPlayer1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btnNewGame);
+            this.Controls.Add(this.btnOnlonePlayers);
+            this.Name = "Battle_Ship";
+            this.Text = "Battle Ship";
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPlayer1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbPlayer2;
+        private System.Windows.Forms.Button btnReady;
+        private System.Windows.Forms.PictureBox pbPlayer1;
+        public System.Windows.Forms.Button block1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnDestory;
+        private System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Button block4;
+        public System.Windows.Forms.Button block3;
+        public System.Windows.Forms.Button block2;
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Button btnOnlonePlayers;
+        private System.Windows.Forms.Button btnStop;
+    }
+}
---
+++ BattleShipService/BattleShipService/IPortal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace BattleShipService
+{
+    [ServiceContract(CallbackContract = typeof(PlayerLogInCallback))]
+    public interface IPortal
+    {
+        [OperationContract]
+        bool Login(string name, string password);
+    }
+    [ServiceContract]
+    public interface PlayerLogInCallback
+    {
+        [OperationContract]
+        void playerLoggedIn(string name);
+    }
+
+}
