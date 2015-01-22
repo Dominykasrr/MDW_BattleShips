f3cfc12d20a74de84bd499382d0b7bf4a5eeadc2
Dominykas <dominykasrr@gmail.com>
2015-01-22 13:23:27 +0100

    add IBattle

---
+++ BattleShipService/BattleShipService/IBattle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace BattleShipService
+{
+    [ServiceContract(Namespace = "BattleShipService", CallbackContract = typeof(IGameCallback))]
+    public interface IBattle
+    {
+        [OperationContract]
+        void ConfirmShips(List<Ship> ships);
+
+        [OperationContract(IsOneWay = true)]
+        void Shoot(int x, int y);
+    
+    }
+    [ServiceContract(Namespace = "BattleShipService")]
+    public interface IGameCallback
+    {
+        void PlayerReady();
+        void NotifyShot(int x, int y);
+    }
+
+}
---
+++ BattleShipService/BattleShipService/Ship.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace BattleShipService
+{
+    public class Ship
+    {
+        public int x;
+        public int y;
+        bool direction;
+        int size;
+
+    }
+
+}
