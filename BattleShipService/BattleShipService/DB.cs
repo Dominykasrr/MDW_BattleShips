4b995fe6177ff736043f1ceaff167ca58decda86
Santi Ram Rai <deewan_me1999@yahoo.com>
2015-01-27 02:24:41 +0100

    What I have done!
    What I have done;
    1. Implemented portal database.
    2. Create DB class.
    3. Create User Registration.
    4. Password Reset.
    5. And more

---
+++ BattleShipService/BattleShipService/DB.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using MySql.Data;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
+
+namespace BattleShipService
+{
+    public class DB
+    {
+        private MySqlConnection conn;
+        private string connectionString;
+
+        public DB()
+        {
+            conn = new MySqlConnection();
+
+            string server = "athena01.fhict.local";
+            string database = "dbi270138";
+            string uid = "dbi270138";
+            string password = "1Lec9taGvr";
+
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" +
+            database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+
+            conn.ConnectionString = connectionString;
+        }
+
+
+        /// <summary>
+        /// This method determines if the username and password combination is sufficient
+        /// for creating a Player object on the server with its appropriate callbacks.
+        /// A return of 1 means player exists, make a log in. 0 means user and password have no match.
+        /// -1 means database-related error.
+        /// </summary>
+        /// <param name="playername"></param>
+        /// <param name="password"></param>
+        /// <returns>0, 1 or -1.</returns>
+        public int CheckLogin(string name, string passwd)
+        {
+            if (CheckUserExist(name) == 1)
+            {
+                try
+                {
+                    String sql = "SELECT username, password FROM mdwpro WHERE username = '" + name + "' AND password = '" + passwd + "';";
+                    MySqlCommand command = new MySqlCommand(sql, conn);
+
+                    conn.Open();
+                    MySqlDataReader reader = command.ExecuteReader();
+
+                    string nm = "";
+                    string pss = "";
+
+                    while (reader.Read())
+                    {
+                        nm = Convert.ToString(reader[0]);
+                        pss = Convert.ToString(reader[1]);
+                    }
+
+                    if (name == nm && passwd == pss)
+                    {
+                        return 1;
+                    }
+                    return 0;
+                }
+                catch (MySqlException)
+                {
+                    return 1;
+                }
+                catch (Exception)
+                {
+                    return 0;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            else if (CheckUserExist(name) == -1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Registers the player in the database. Does not equate to being logged in.
+        /// A return of false means the playerusername already exists in the database.
+        /// </summary>
+        /// <param name="firstname"></param>
+        /// <param name="lastname"></param>
+        /// <param name="playername"></param>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        /// <returns>Number of records changed. -1 means unsuccessful execution of try-block.</returns>
+        public bool UserRegister(string name, string passwd)
+        {
+            if (this.CheckUserExist(name) == 0)
+            {
+                String sql = "INSERT INTO mdwpro (username, password) VALUES ('" + name + "','" + passwd + "');";
+                MySqlCommand command = new MySqlCommand(sql, conn);
+
+                try
+                {
+                    conn.Open();
+                    int nrOfRecordsChanged = command.ExecuteNonQuery();
+                    return true;
+                }
+                catch
+                {
+                    return false;
+                }
+                finally
+                {
+                    conn.Close();
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Returns the number player in database. Return of 0 means does not exist.
+        /// Should not return more than 1.
+        /// </summary>
+        /// <param name="playername"></param>
+        /// <returns>Successfully either 0 or 1</returns>
+        public int CheckUserExist(string name)
+        {
+            try
+            {
+                String sql = ("SELECT username FROM mdwpro WHERE username = '" + name + "';");
+                MySqlCommand command = new MySqlCommand(sql, conn);
+
+                conn.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+
+                int count = 0;
+
+                while (reader.Read())
+                {
+                    count++;
+                }
+                return count;
+            }
+            catch (MySqlException)
+            {
+                return 0;
+            }
+            catch (Exception)
+            {
+                return 0;
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+        /// <summary>
+        /// This method checks if the player name is exist in the database or not. And reset the new password in the database.
+        /// </summary>
+        /// <param name="name">Player name</param>
+        /// <param name="passwd">Player password</param>
+        /// <returns>true or false</returns>
+        public bool CheckResetPassword(string name, string passwd)
+        {
+            if (this.CheckUserExist(name) == 1)
+            {
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                MySqlCommand cmd;
+                connection.Open();
+
+                try
+                {
+                    cmd = connection.CreateCommand();
+                    cmd.CommandText = "UPDATE mdwpro SET password=@passwd WHERE username=@name";
+                    cmd.Parameters.AddWithValue("@name", name);
+                    cmd.Parameters.AddWithValue("@passwd", passwd);
+                    cmd.ExecuteNonQuery();
+                    return true;
+                }
+                catch (MySqlException)
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+    }
+}
--- BattleShipService/BattleShipService/GameService.cs
+++ BattleShipService/BattleShipService/GameService.cs
@@ -4,42 +4,37 @@ using System.Linq;
 using System.Runtime.Serialization;
 using System.ServiceModel;
 using System.Text;
+using System.Data;
+using MySql.Data;
+using MySql.Data.Common;
+using MySql.Data.MySqlClient;
 
 namespace BattleShipService
 {
     [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, ConcurrencyMode = ConcurrencyMode.Reentrant)]
-    public class GameService : IPortal, IChat, IBattle
+    public class GameService : IPortal, IChat
     {
-        static Action<List<Player>> PlayerLogIn_Event = delegate { };
-        static Action<String> Invitation_Send_Event = delegate { };
-        static Action<Game> Game_Start_Event = delegate { };
-
+        static Action<List<Player>> PlayerLogIn_Event = delegate { }; //Player login event.
+        static Action<String> Invitation_Send_Event = delegate { }; //Invitation send event.
+        static Action<Game> Game_Start_Event = delegate { }; //Game start event.
 
         public List<Player> onlinePlayers;
         public List<Player> registeredPlayers;
+        public List<Player> players;
         List<Game> gamesLists;
 
         public GameService()
         {
             onlinePlayers = new List<Player>();
-            registeredPlayers = new List<Player>();
+            //registeredPlayers = new List<Player>();
             gamesLists = new List<Game>();
+            //players = new List<Player>();
 
-
-            registeredPlayers.Add(new Player("tool", "bar"));
-            registeredPlayers.Add(new Player("bar", "tool"));
-            registeredPlayers.Add(new Player("aa", "bb"));
+            //registeredPlayers.Add(new Player("tool", "bar"));
+            //registeredPlayers.Add(new Player("bar", "tool"));
+            //registeredPlayers.Add(new Player("aa", "bb"));
         }
 
-<<<<<<< HEAD
-        public GameService(InstanceContext instanceContext)
-        {
-            // TODO: Complete member initialization
-            this.instanceContext = instanceContext;
-        }
-
-<<<<<<< HEAD
-<<<<<<< HEAD
         /// <summary>
         /// This logIn methods is going to call "CheckLogin" methods from the DB class.
         /// If there is player found with that username and password from the parameter, 
@@ -51,45 +46,81 @@ namespace BattleShipService
         /// <param name="passwd">Player password</param>
         /// <returns>true or false</returns>
         public bool Login(int Id, string name, string passwd)
-=======
-        public bool Login(string name, string passwd)
->>>>>>> origin/master
-=======
-        public bool Login(string name, string passwd)
->>>>>>> parent of 2143dec... What I have done; 1.	Implemented portal database. 2.	Create DB class. 3.	Create User Registration. 4.	Password Reset. 5.	And more
-=======
-        public bool Login(string name, string passwd)
->>>>>>> parent of 2143dec... What I have done; 1.	Implemented portal database. 2.	Create DB class. 3.	Create User Registration. 4.	Password Reset. 5.	And more
         {
-            foreach (Player p in registeredPlayers)
-            {
-                if (p.name == name && p.passwd == passwd)
-                {
-                    p.IPortal_Callback = OperationContext.Current.GetCallbackChannel<IPortalCallback>();
-                    onlinePlayers.Add(p);
-                    PlayerLogIn_Event += p.IPortal_Callback.PlayerLoggedIn;
-                    //Subscribing to events.
+            DB dbhelper = new DB();
 
-                    return true;
-                }
+            if (dbhelper.CheckLogin(name, passwd) == 1)
+            {
+                Player tempPlayer;
+                tempPlayer = new Player(1,name,passwd) ;
+                tempPlayer.IPortal_Callback = OperationContext.Current.GetCallbackChannel<IPortalCallback>();
+                onlinePlayers.Add(tempPlayer);
+                PlayerLogIn_Event += tempPlayer.IPortal_Callback.PlayerLoggedIn;
+                return true;
             }
             return false;
         }
 
+        /// <summary>
+        /// This method will call the "UserRegister" method form class DB.
+        /// And pass the data to the database from the parameter.
+        /// </summary>
+        /// <param name="Id">Player ID</param>
+        /// <param name="name">Player name</param>
+        /// <param name="passwd">Player password</param>
+        /// <returns></returns>
+        public bool UserRegister(int Id, string name, string passwd)
+        {
+            DB dbhelper = new DB();
+            return dbhelper.UserRegister(name, passwd);
+        }
+
+        /// <summary>
+        /// This method will call the "CheckResetPassword" method form class DB.
+        /// And pass the data to the database from the parameter.
+        /// </summary>
+        /// <param name="Id">Player name</param>
+        /// <param name="name">Player password</param>
+        /// <returns></returns>
+        public bool ResetPassword(string name, string passwd)
+        {
+            DB dbhelper = new DB();
+            return dbhelper.CheckResetPassword(name, passwd);
+        }
+
+        /// <summary>
+        /// This method will return the all online players.
+        /// </summary>
+        /// <returns>onlinePlayers</returns>
         public List<Player> GetOnlinePlayer()
         {
             return onlinePlayers;
         }
 
 
+        /// <summary>
+        /// I am creating playerinvitee and i am passing the paramenter p1 to it, 
+        /// and aclling the event Invitation_Send_Event to IPortal_Callback NotifyChallenge 
+        /// and add the parameter p2(other palyer) to invite.
+        /// </summary>
+        /// <param name="p1">Player1</param>
+        /// <param name="p2">Player2</param>
         public void InvitePlayer(string p1, string p2)
         {
             Player playerinvitee = GetPlayer(p1);
-            // playerinvitee.IPortal_Callback.NotifyChallenge(p2);
+            
             Invitation_Send_Event += playerinvitee.IPortal_Callback.NotifyChallenge;
             Invitation_Send_Event(p2);
         }
 
+        /// <summary>
+        /// Here i am creating two temporary players player1 and player2, and i am passing the parameter to it.
+        /// And also i am crating new game, and i am paiing the player1 and player2 to the game.
+        /// And firing the event "Game_Start_Event" to notify the challenge Callback. To player1 and player2.
+        /// And i have fire "Game_Start_Event" to game.
+        /// </summary>
+        /// <param name="p1">Player1</param>
+        /// <param name="p2">Player2</param>
         public void AcceptInvitation(string p1, string p2)
         {
             Player player1 = GetPlayer(p1);
@@ -99,9 +130,13 @@ namespace BattleShipService
             Game_Start_Event += player1.IPortal_Callback.NotifyResponce;
             Game_Start_Event += player2.IPortal_Callback.NotifyResponce;
             Game_Start_Event(game);
-
         }
 
+        /// <summary>
+        /// If the player is equal to playername give parameter, than returns the player.
+        /// </summary>
+        /// <param name="playerusername">Player username</param>
+        /// <returns>null</returns>
         public Player GetPlayer(string playerusername)
         {
             foreach (Player players in this.onlinePlayers)
@@ -114,37 +149,48 @@ namespace BattleShipService
             return null;
         }
 
+        /// <summary>
+        /// This update methods will fire the(PlayerLogIn_Event) PlayerLogIn Event.
+        /// </summary>
         public void Update()
         {
             PlayerLogIn_Event(onlinePlayers);
         }
 
-
+        /// <summary>
+        /// This method is for posting the chat message between two players.
+        /// I ma creating instance of Type player, and assign it to the opponent from the parameter.
+        /// And here i am calling the UpdateChatMessages method and attaching IchatCallBack to it.
+        /// </summary>
+        /// <param name="message">Message type by player.</param>
+        /// <param name="postername">Invitee player</param>
+        /// <param name="opponent">Opponent Player</param>
         public void PostChatMessage(string message, string postername, string opponent)
         {
             Player p = GetPlayer(opponent);
             p.IchatCallBack.UpdateChatMessages(message, postername);
-
         }
 
+        /// <summary>
+        /// This method is for star chat session.
+        /// In this method i ma calling "Start" method, and i have pass the parameter player to it.
+        /// </summary>
+        /// <param name="player">Player to start</param>
         public void StartChatSession(string player)
         {
             Start(player);
         }
+
+        /// <summary>
+        /// This method is to Subscribe the chat funcation.
+        /// Here i have created the object p1 of Type Player. And passing the player from the parameter.
+        /// After that i have called the callbak IchatCallBack to it.
+        /// </summary>
+        /// <param name="player"></param>
         public void Start(string player)
         {
             Player p1 = GetPlayer(player);
             p1.IchatCallBack = OperationContext.Current.GetCallbackChannel<IChatCallback>();
-        }
-
-        public void ConfirmShips(List<Ship> ships)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Shoot(int x, int y)
-        {
-            throw new NotImplementedException();
         }
     }
 }
